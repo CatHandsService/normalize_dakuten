06c2c91c872da39c6cb296a90637c05cdb10ea86
cathands <cathandsservice@gmail.com>
2024-01-31 02:02:19 +0900

    cors change

--- back/main.py
+++ back/main.py
@@ -11,7 +11,7 @@ load_dotenv(override=True)
 
 app = Flask(__name__, template_folder="./templates", static_folder="./static")
 origins = [
-    "https://normalize-dakuten-jzlg.vercel.app",
+    "https://normalize-dakuten-jzlg-q6n6djj4i-cathandsservice.vercel.app",
     "http://localhost:5173",
 ]
 CORS(app, origins=origins)
